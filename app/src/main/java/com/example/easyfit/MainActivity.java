d5e70cd2721c36804d91a36336197483b38d3bb0
JoshR09 <josh.ryan3464@gmail.com>
2023-06-18 20:07:41 +1000

    Removed action bar

--- app/src/main/java/com/example/easyfit/MainActivity.java
+++ app/src/main/java/com/example/easyfit/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.easyfit;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,11 +31,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
     }
